b212fb515153453eafcfb9612f70b8ee33d14df5
Soliloquiess <cyh1219@naver.com>
2022-09-02 10:42:23 +0900

    feat(yacho)[add]: mybatis, oracle 연동. 그리고 연동할 떄 디비 대소문자도 구분 중요. 나같은 경우 컨트롤러에 소문자로 적으니까 디비 저장은 자동 대문자인지 몰라도 소문자일떄는 값 못 가져 오더니 대문자일떄는 값을 가져왔다

--- SpringInteliJ/Self_Study/SpringbootRecord/src/main/java/com/spring/boot/controller/Home.java
+++ SpringInteliJ/Self_Study/SpringbootRecord/src/main/java/com/spring/boot/controller/Home.java
@@ -1,19 +1,25 @@
 package com.spring.boot.controller;
 
+import com.spring.boot.service.StudyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/home")
 public class Home {
+
+    @Autowired
+    StudyService studyService;
 
     @GetMapping("")
     public String doHome(){
@@ -22,11 +28,26 @@ public class Home {
     }
 
     @GetMapping("/study_reg")
+    @ResponseBody
     public String doStudy_reg(){
+            List<Map<String,String>> list = new ArrayList<>();
+            list = studyService.doStudyList();
 
+        //xml에서 설정 안하면 디비 있는 이름 그대로 써야
+            for (Map<String, String> map : list){
+                System.out.println(map.get("KEY_ID"));
+                System.out.println(map.get("STUDY_DAY"));
+                System.out.println(map.get("CONTENTS"));
+                System.out.println(map.get("REG_DAY"));
+            }
 
-        return "/study/study_reg";
+            return "--";
+
     }
+
+
+
+
 
     @GetMapping("/member_list")
     public String doMember_list(){
