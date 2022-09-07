e05113085b7e622353795b02b68577b69905006f
yacho <cyh1219@naver.com>
2022-09-07 15:21:04 +0900

    feat(yacho)[update]: 로그인 아이디 안쓰면 에러나던거, 비번 달라도 홈 화면 가는거 수정

--- SpringInteliJ/Self_Study/SpringbootRecord/src/main/java/com/spring/boot/controller/Login.java
+++ SpringInteliJ/Self_Study/SpringbootRecord/src/main/java/com/spring/boot/controller/Login.java
@@ -26,8 +26,8 @@ public class Login {
     @PostMapping("/login_exe")
     public String doLoginExe(@RequestParam(value="loginId", defaultValue = "--") String strLoginId,
                              @RequestParam(value="password", defaultValue = "--") String strPassword,
-                             Model model
-                             ) {
+                             Model model,
+                              HttpServletRequest request){
 
         String strReturn ="";
         String strMessage="";
@@ -35,21 +35,24 @@ public class Login {
         Vo_member vo_member = memberService.doMemberLogin(strLoginId);
 
         if(vo_member==null){
+            System.out.println("d없음");
             strReturn = "/login/loginForm";
             strMessage = "ID가 존재하지 않아요.";
         }else{
             if(!strPassword.equals(vo_member.getPassword())) {
                 strReturn = "/login/loginForm";
                 strMessage = "패스워드가 일치하지 않아요.";
+            }//else 없으면 패스워드 없어도 성공해서 수정
+            else{
+                strReturn = "redirect:/";
+                strMessage="login성공";
             }
-            strReturn = "redirect:/";
-            strMessage = "login성공";
 
         }
 
         model.addAttribute("message", strMessage);
-
-        System.out.println(vo_member.getName());
+        HttpSession session = request.getSession();
+//        System.out.println(vo_member.getName());//vo가 null이면 오류남.
 //        return "hi";
         return strReturn;
     }
--- SpringInteliJ/Self_Study/SpringbootRecord/src/main/java/com/spring/boot/vo/Vo_member.java
+++ SpringInteliJ/Self_Study/SpringbootRecord/src/main/java/com/spring/boot/vo/Vo_member.java
@@ -14,6 +14,16 @@ public class Vo_member {
      private String  role;
      private String  regDay;
 
+
+    public Vo_member(String memberId, String loginId, String password, String name, String role, String regDay) {
+        this.memberId = memberId;
+        this.loginId = loginId;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+        this.regDay = regDay;
+    }
+
     @Builder
     public Vo_member( String loginId, String password, String name, String role, String regDay) {
         this.loginId = loginId;
